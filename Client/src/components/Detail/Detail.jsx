b6a9791b2c86b2b48ffc7dc1a1d886264b7a021d
JuanD <juanxd3009@gmail.com>
2023-08-25 23:35:48 -0500

    server localhost3001

---
+++ Client/src/components/Detail/Detail.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import style from "./Detail.module.css";
+// import Swal from "sweetalert2";
+
+const Detail = () => {
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios(`http://localhost:3001/rickandmorty/character/${id}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacter(data);
+          setLoading(false);
+        }
+        // else {
+        //   Swal.fire({
+        //     icon: "error",
+        //     title: "Oops...",
+        //     text: "¡Debes ingresar un ID!",
+        //   });
+        // }
+      }
+    );
+    return setCharacter({});
+  }, [id]);
+
+  // useEffect(() => {
+  //   axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+  //     ({ data }) => {
+  //       if (data.name) {
+  //         setCharacter(data);
+  //         setLoading(false);
+  //       }
+  //       // else {
+  //       //   Swal.fire({
+  //       //     icon: "error",
+  //       //     title: "Oops...",
+  //       //     text: "¡Debes ingresar un ID!",
+  //       //   });
+  //       // }
+  //     }
+  //   );
+  //   return setCharacter({});
+  // }, [id]);
+
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className={style.container}>
+      <h2>Name: {character.name}</h2>
+      <h2>Status: {character.status}</h2>
+      <h2>Specie: {character.species}</h2>
+      <h2>Gender: {character.gender}</h2>
+      <h2>Origin: {character.origin.name}</h2>
+      <img src={character.image} alt={character.name} />
+    </div>
+  );
+};
+
+export default Detail;
---
+++ Server/src/index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const data = require("./utils/data");
+
+http
+  .createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const { url } = req;
+
+    if (url.includes("/rickandmorty/character/")) { //todo lo que yo ponga dentro del if se va a ejecutar TODO!!!
+      const id = url.split("/").at(-1);
+      const character = data.find((char) => char.id == id); 
+      //* con find ingresamos al array data y retornamos el primer elemento que cumpla
+    //   if (character) {
+        res.writeHead(200, { "Content-Type": "application/json" }); //* estatus 200 es una respuesta 'OK'
+        return res.end(JSON.stringify(character)); //* con esta linea de codigo yo convierto el array js a json para servers
+    //   }
+    }
+  })
+  .listen(3001, "localhost"); //! OJO nunca olvidar el listen
